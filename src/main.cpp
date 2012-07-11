6145358a4564177dfd4df1af9d444d6680623a30
Fredrik Nyström <nfa106@gmail.com>
2012-07-11 16:36:02 +0200

    Added timings.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 //#include "config/YggdrasilConfig.h"
 #include "LPC/input.h"
 #include "LPC/Grammar/lexer.h"
@@ -22,29 +23,52 @@ int main() {
     "";
 //  std::ifstream data2("tokens.txt");
 //  std::ifstream data3("tokens2.txt");
+  std::vector<LPC::Input*> data;
+  long long tokens = 0;
+  long long iterations = 0;
   
-  LPC::Input input1(data1);
+  std::chrono::steady_clock::time_point tend = std::chrono::steady_clock::now()
+                                               + std::chrono::seconds(30);
+  
+  std::cout << "Starting lexing (30 seconds) ..." << std::endl;
+  while (std::chrono::steady_clock::now()  < tend) {
+    LPC::Input *input = new LPC::Input(data1);
+    LPC::Grammar::Lexer *lexer = new LPC::Grammar::Lexer(*input);
+    lexer->Lex();
+    tokens += lexer->CountTokens();
+    delete lexer;
+    delete input;
+    ++iterations;
+  }
+    
+    
+//  LPC::Input input1(data1);
 //  LPC::Input input2(data2);
 //  LPC::Input input3(data3);
    
   header();
 
-  LPC::Grammar::Lexer lex1(input1);
+//  LPC::Grammar::Lexer lex1(input1);
 //  LPC::Grammar::Lexer lex2(input2);
 //  LPC::Grammar::Lexer lex3(input3);
   
-  lex1.Lex();
+//  lex1.Lex();
 //  lex2.Lex();
 //  lex3.Lex();
  
-  std::cout << " lex1 size = " << lex1.CountTokens() << std::endl;
+//  std::cout << " lex1 size = " << lex1.CountTokens() << std::endl;
 //  std::cout << " lex2 size = " << lex2.CountTokens() << std::endl;
 //  std::cout << " lex3 size = " << lex3.CountTokens() << std::endl;
   std::cout << std::endl;
+  
+  long long bytes_per_sec = (strlen(data1)*iterations)/30;
+  double megabytes_per_sec = ((double)bytes_per_sec)/1024/1024;
 
-  std::cout << std::endl;
+  std::cout << "Done." << std::endl;
+  std::cout << tokens/30 << " tokens/sec." << std::endl;
+  std::cout << megabytes_per_sec << " Mb/sec." << std::endl;
 //  lex1.DumpTokens();
-  lex1.DumpTokens();
+//  lex1.DumpTokens();
 
 }
 
